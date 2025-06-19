3f2091a5f85341cd55d9de99eb2369fff542834f
sja1383 <sja1383@wgu.edu>
2025-06-19 15:15:40 -0400

    Almost done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Hero from './components/Hero.jsx'
 import Header from './components/Header.jsx'
 import Navbar from "./components/Navbar.jsx";
+import Skills from "./components/Skills.jsx";
+import Projects from "./components/Projects.jsx";
 
 const App = () => {
     return (
         <div className="bg-white text-black dark:bg-black dark:text-gray-300 min-h-screen transition-colors duration-300">
             <Navbar />
             <Hero />
+            <Skills />
+            <Projects />
         </div>
     )
 }
